5fd4ce1231893b504fa0f42ab5eaf0b3d070f700
Yuan <wryyy0717@gmail.com>
2019-11-04 16:45:34 -0600

    tarea 7

---
+++ TAREAS/7/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc, char* argu[]){
+	int x;
+	int y;
+	float z=1;
+	x=atof(argu[1]);
+	y=atof(argu[2]);
+	for (int i=0; i<y; i++){
+		z=z*x;
+	}
+	printf("%.2f\n",z);
+	return 0;
+}
